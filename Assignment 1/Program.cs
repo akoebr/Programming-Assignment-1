1169268805a8ed1553daa6506ecaf53a319f1f0f
akoebr <ako_ab@hotmail.com>
2015-03-06 15:29:27 +0000

    Secoond Version

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -14,7 +14,31 @@ namespace Assignment_1
         }
         public static void PrintMesssage()
         {
-            Console.WriteLine("A C# Method");
+            Console.WriteLine("Welcome to my first c# Program");
+            float theta;
+            float speed;
+            float vox;
+            float voy;
+            double time;
+            double height;
+            double dx;
+            const double GRAVITY = 9.8;
+
+
+            Console.WriteLine("Welcome Please enter the angle Theta:  ");
+            theta = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Speed:  ");
+            speed = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("You have entered: " + speed);
+            vox = speed * (float)Math.Cos(theta);
+            voy = speed * (float)Math.Sin(theta);
+            time = (double)(voy / GRAVITY);
+            height = voy * voy / (2 * GRAVITY);
+            dx = vox * 2 * time;
+            Console.WriteLine(" VOX: " + vox + "\n voY: " + voy + "\nTime: " + time + "\n   h: " + height + "\n  dx: " + dx);
+            
         }
     }
     
