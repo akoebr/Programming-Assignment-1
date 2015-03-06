ecfb9ed64dc9b6cf534366017a9c24e0f0a50cbb
akoebr <ako_ab@hotmail.com>
2015-03-06 15:09:53 +0000

    Assignment 1

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMesssage();
+        }
+        public static void PrintMesssage()
+        {
+            Console.WriteLine("A C# Method");
+        }
+    }
+    
+}
